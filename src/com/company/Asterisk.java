00c41b791ad3bc472980f5fd2519a6e0a252bb52
Mrinal Kanti Ghosh <mrinal2196ghosh@gmail.com>
2018-08-03 09:41:26 +0530

    Asterisk class created

---
+++ src/com/company/Asterisk.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Asterisk {
+    private String asterisk;
+
+    Asterisk(){
+        this.asterisk = "*";
+    }
+
+    public String getAsterisk(){
+        return this.asterisk;
+    }
+}
